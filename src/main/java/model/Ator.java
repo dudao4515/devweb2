255c9b4e554f15bedeb5b38e8182bd09b5b89a8c
dudao4515 <95761034+dudao4515@users.noreply.github.com>
2025-09-02 16:40:35 -0300

    Initial commit

---
+++ src/main/java/model/Ator.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Duda
+ */
+public class Ator {
+    private String nome;
+
+    public Ator(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
